45b5da096bbfa87be5a096bc34e7e8a7b5d27628
Liam O'Donnell <liam.odonnell@tapcheck.com>
2022-02-09 20:35:49 -0800

    updating website with new links to muci

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { FaYoutube, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { FaYoutube, FaInstagram, FaTwitter, FaMusic } from 'react-icons/fa'
+import '../styles/linkpage.css'
 import '../styles/index.css'
 
 export default function Home() {
@@ -8,13 +9,14 @@ export default function Home() {
     <Layout>
       <section className='flex-center-column'>
         <section className="title flex-center-column">
-          <div class="me"></div>
+          <div class="profile-image me"></div>
           <h1>Liam O'Donnell</h1>
         </section>
         <section className="links flex-center-column">
           <a className="youtube" href="https://www.youtube.com/channel/UCg7Z9p23feEGEu9JnJPOrrg"><FaYoutube/>Youtube</a>
           <a className="instagram" href="https://www.instagram.com/imliamodonnell/"><FaInstagram/>Instagram</a>
           <a className="twitter" href="https://twitter.com/imliamodonnell"><FaTwitter/>Twitter</a>
+          <a className="music" href="https://links.luabeta.com"><FaMusic/>Music</a>
         </section>
       </section>
     </Layout>
