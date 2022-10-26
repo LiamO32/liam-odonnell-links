cf33a438972241f04c4fb573bb376b56bbf4d1e5
Liam O'Donnell <liam.odonnell@tapcheck.com>
2022-10-25 19:38:29 -0700

    update profile image and theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { FaYoutube, FaInstagram, FaTwitter, FaMusic } from 'react-icons/fa'
+import { FaYoutube, FaInstagram, FaTwitter, FaMusic, FaLinkedin } from 'react-icons/fa'
 import '../styles/linkpage.css'
 import '../styles/index.css'
 
-export default function Home() {
-  return (
-    <Layout>
-      <section className='flex-center-column'>
-        <section className="title flex-center-column">
-          <div className="profile-image me"></div>
-          <h1>Liam O'Donnell</h1>
+export default class Home extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section className='flex-center-column'>
+          <section className="title flex-center-column">
+            <div className="profile-image me"></div>
+            <h1>Liam O'Donnell</h1>
+          </section>
+          <section className="links flex-center-column">
+            <a className="linkedin" href="https://www.linkedin.com/in/imliamodonnell/"><FaLinkedin/>LinkedIn</a>
+            <a className="youtube" href="https://www.youtube.com/channel/UCg7Z9p23feEGEu9JnJPOrrg"><FaYoutube/>Youtube</a>
+            <a className="instagram" href="https://www.instagram.com/imliamodonnell/"><FaInstagram/>Instagram</a>
+            <a className="twitter" href="https://twitter.com/imliamodonnell"><FaTwitter/>Twitter</a>
+            <a className="music" href="https://links.luabeta.com"><FaMusic/>Music</a>
+          </section>
         </section>
-        <section className="links flex-center-column">
-          <a className="youtube" href="https://www.youtube.com/channel/UCg7Z9p23feEGEu9JnJPOrrg"><FaYoutube/>Youtube</a>
-          <a className="instagram" href="https://www.instagram.com/imliamodonnell/"><FaInstagram/>Instagram</a>
-          <a className="twitter" href="https://twitter.com/imliamodonnell"><FaTwitter/>Twitter</a>
-          <a className="music" href="https://links.luabeta.com"><FaMusic/>Music</a>
-        </section>
-      </section>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
